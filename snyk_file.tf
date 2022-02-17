afa8051c2baff5a50d2d303e7c9cfad1f5d707d8
aluguritarun3006 <99875462+aluguritarun3006@users.noreply.github.com>
2022-02-18 00:07:52 +0530

    Update snyk_file.tf

--- snyk_file.tf
+++ snyk_file.tf
@@ -11,3 +11,18 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "b2" {
+  bucket = "my-tf-test-bucket2"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
